f617984284683e85bad27907e0bfa07603ab24b9
Paranietharan <paranietharan64@gmail.com>
2024-06-24 14:19:21 +0530

    Comment conected for article

--- src/Components/Comment.js
+++ src/Components/Comment.js
@@ -1,17 +1,21 @@
 import React from "react";
 
 function Comment({ comment }) {
-    const { imgLink, author, content, timestamp } = comment;
+    const { comment: content, commenterId: authorId, articleId, timestamp } = comment;
 
     return (
         <>
             <div style={{ padding: "20px 10px", display: "flex", alignItems: "center" }}>
-                <img src={imgLink} alt={author} style={{ borderRadius: "50%", width: "50px", height: "50px", marginRight: "10px" }} />
                 <div style={{ flex: "1", minWidth: "0" }}>
-                    <h4 style={{ margin: 0, textAlign: "left" }}>{author}</h4>
                     <p style={{ textAlign: "left" }}>{content}</p>
                     <p style={{ textAlign: "left", color: "gray" }}>
-                        posted {timestamp}
+                        posted on {timestamp}
+                    </p>
+                    <p style={{ textAlign: "left", color: "gray" }}>
+                        Commenter ID: {authorId}
+                    </p>
+                    <p style={{ textAlign: "left", color: "gray" }}>
+                        Article ID: {articleId}
                     </p>
                 </div>
             </div>
