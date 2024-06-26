ab999d9e23d81d66c74daaa3b0ac1dd71e6339cd
Paranietharan <paranietharan64@gmail.com>
2024-06-26 14:48:57 +0530

    Dependecies solved && scroll problem solved

--- src/Pages/PublishArticles.js
+++ src/Pages/PublishArticles.js
@@ -1,55 +1,57 @@
 import React, { useState } from 'react';
 import ArticleNavBar from '../Components/ArticleNavBar';
 import Footer from '../Components/LibraryFooter';
-import { EditorState, convertToRaw } from 'draft-js';
-import { Editor } from 'react-draft-wysiwyg';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import axios from 'axios';
 
 function ArticleForm() {
     const [image, setImage] = useState(null);
     const [heading, setHeading] = useState('');
-    const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+    const [body, setBody] = useState('');
 
     const handleImageChange = (e) => {
         const selectedImage = e.target.files[0];
         setImage(selectedImage);
     };
 
-    const handleSubmit = (e) => {
+    const handleBodyChange = (e) => {
+        setBody(e.target.value);
+    };
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
-        // Extract the raw content state from the editor state
-        const contentState = editorState.getCurrentContent();
-        const rawContentState = convertToRaw(contentState);
+        const formData = new FormData();
+        formData.append('title', heading);
+        formData.append('body', body);
+        formData.append('authorId', '1');// Hardcoded for now
 
-        // Extract the highlighted text from the raw content state
-        let highlightedText = '';
-        rawContentState.blocks.forEach(block => {
-            if (block.inlineStyleRanges && block.inlineStyleRanges.length > 0) {
-                block.inlineStyleRanges.forEach(style => {
-                    if (style.style === 'HIGHLIGHT') {
-                        highlightedText += block.text.substring(style.offset, style.offset + style.length) + ' ';
-                    }
-                });
-            }
-        });
+        if (image) {
+            formData.append('articleImg', image);
+        } else{
+            formData.append('articleImg', null);
+        }
 
-        // Log all the data including highlighted content
-        console.log('Form submitted:', {
-            image,
-            heading,
-            content: rawContentState,
-            highlightedText: highlightedText.trim(), // Trim to remove any leading/trailing whitespace
-        });
+        try {
+            const response = await axios.post('http://localhost:8080/article/addArticle', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+            console.log(formData)
 
-        // Reset form fields after submission
-        setImage(null);
-        setHeading('');
-        setEditorState(EditorState.createEmpty());
+            console.log(response.data);
+
+            // set editor to default values
+            setHeading('');
+            setBody('');
+            setImage(null);
+        } catch (error) {
+            console.error('Error submitting form:', error);
+        }
     };
 
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', overflowY: 'scroll' }}>
             <ArticleNavBar />
 
             <div style={{
@@ -63,6 +65,9 @@ function ArticleForm() {
             }}>
                 <div style={{ maxWidth: '80%', margin: '0 auto' }}>
                     <form onSubmit={handleSubmit}>
+                        <div style={{ textAlign: 'right', marginBottom: '20px' }}>
+                            <button type="submit" style={{ padding: '10px 20px', backgroundColor: '#007bff', color: '#fff', border: 'none', borderRadius: '5px', cursor: 'pointer', fontSize: '1.2rem' }} className="submit-button">Publish</button>
+                        </div>
                         <div style={{ marginBottom: '20px' }}>
                             <input
                                 type="text"
@@ -75,58 +80,41 @@ function ArticleForm() {
                         </div>
 
                         <div style={{ marginBottom: '20px' }}>
-                            <input
-                                type="text"
-                                value={heading}
-                                onChange={(e) => setHeading(e.target.value)}
-                                placeholder="#Tags"
-                                style={{ width: '100%', padding: '10px', marginBottom: '15px', border: '1px solid #ccc', borderRadius: '5px', boxSizing: 'border-box', fontSize: '1.2rem' }}
-                                className="title-input"
-                            />
-                        </div>
-
-                        <div style={{ marginBottom: '20px' }}>
                             <label htmlFor="image" style={{ display: 'block', marginBottom: '5px', fontSize: '1.2rem', fontWeight: 'bold' }}>Image:</label>
                             <input
                                 type="file"
                                 id="image"
                                 accept="image/*"
                                 onChange={handleImageChange}
-                                style={{ width: '50%', padding: '10px', marginBottom: '15px', border: '1px solid #ccc', borderRadius: '5px', boxSizing: 'border-box', fontSize: '1.2rem'  }}
+                                style={{ width: '50%', padding: '10px', marginBottom: '15px', border: '1px solid #ccc', borderRadius: '5px', boxSizing: 'border-box', fontSize: '1.2rem' }}
                                 className="image-input"
                             />
-
-                            {image && (
-                                <img
-                                    src={URL.createObjectURL(image)}
-                                    alt="Preview"
-                                    style={{ width: '100%', marginBottom: '15px', borderRadius: '5px' }}
-                                    className="image-preview"
-                                />
-                            )}
                         </div>
+
+                        {image && (
+                            <img
+                                src={URL.createObjectURL(image)}
+                                alt="Preview"
+                                style={{ width: '100px', marginBottom: '15px', borderRadius: '5px', padding: '10px' }}
+                                className="image-preview"
+                            />
+                        )}
 
                         <div style={{ marginBottom: '20px' }}>
-                            <div style={{ border: '1px solid #ccc', borderRadius: '5px', minHeight: '200px' }}>
-                                <Editor
-                                    editorState={editorState}
-                                    wrapperClassName="editor-wrapper"
-                                    editorClassName="editor-content"
-                                    onEditorStateChange={setEditorState}
-                                    placeholder="Enter your article content"
-                                />
-                            </div>
-                        </div>
+                            <textarea
+                                value={body}
+                                onChange={handleBodyChange}
+                                style={{ border: '1px solid #ccc', borderRadius: '5px', minHeight: '200px', width: '70vw', overflowY: 'scroll' }}
+                                placeholder="Type your article body here"
+                            />
 
-                        <div style={{ textAlign: 'right'}}>
-                            <button type="submit" style={{ padding: '10px 20px', backgroundColor: '#007bff', color: '#fff', border: 'none', borderRadius: '5px', cursor: 'pointer', fontSize: '1.2rem' }} className="submit-button">Publish</button>
                         </div>
                     </form>
                 </div>
             </div>
 
             <Footer />
-        </div >
+        </div>
     );
 }
 
