687ece3eb16c8ce7805d6d223b509be3b2c7c9b4
Paranietharan <paranietharan64@gmail.com>
2024-07-06 02:29:40 +0530

    Return book work

--- src/Pages/BookLending.js
+++ src/Pages/BookLending.js
@@ -1,53 +1,16 @@
-import { useState } from 'react';
 import LibrarianTopNavBar from '../Components/LibrarianTopNavBar';
 import Style from './style/BookLending.module.css';
-import MemberSearchComponent from '../Components/MemberSearchComponent';
-import BookSearchComponent from '../Components/BookSearchComponent';
 import Footer from '../Components/LibraryFooter';
-import AlertMessage from '../Components/AlertMessage';
-import http from '../service/http-common';
+import AdminBookIssue from '../Components/AdminBookIssue';
+import { useState } from 'react';
+import AdminBookGetReturn from '../Components/AdminBookGetReturn';
 
 function BookLending() {
-    const [selectedMember, setSelectedMember] = useState(null);
-    const [selectedBook, setSelectedBook] = useState(null);
-    const [showError, setShowError] = useState(false);
 
-    const handleSelectMember = (member) => {
-        setSelectedMember(member);
-    };
+    const [displayComponent, setDisplayComponent] = useState('lend');
 
-    const handleSelectBook = (book) => {
-        setSelectedBook(book);
-    };
-
-    const handleLendBook = async () => {
-        if (!selectedMember || !selectedBook) {
-            setShowError(true);
-            return;
-        }
-
-        try {
-            const response = await http.post(`/issues/issue?resourceId=${selectedBook.resourceId}&memberId=${selectedMember.userID}`);
-            console.log("Book issued successfully:", response.data);
-            // Optionally, you can reset selectedMember and selectedBook state here
-
-            // reset the book and member state
-            setSelectedBook(null);
-            setSelectedMember(null);
-
-            // Show success message
-            alert("Book issued successfully!");
-
-            // forwards to the book lending page
-            window.location.href = "/book-lending";
-        } catch (error) {
-            console.error("Error issuing book:", error);
-            // Handle error
-        }
-    };
-
-    const handleCloseError = () => {
-        setShowError(false);
+    const handleButtonClick = (componentName) => {
+        setDisplayComponent(componentName);
     };
 
     return (
@@ -56,31 +19,15 @@ function BookLending() {
                 <LibrarianTopNavBar />
             </div>
             <div className={Style.container}>
-
-                <div className={Style.content}>
-                    <div className={Style.MemberSearch}>
-                        <MemberSearchComponent onSelectMember={handleSelectMember} />
-                    </div>
-
-                    <div className={Style.BookSearch}>
-                        <BookSearchComponent onSelectBook={handleSelectBook} />
-                    </div>
-
+                <div className={Style.menuItems}>
+                    <button className={Style.lend} onClick={() => handleButtonClick('lend')}>Lend Books</button>
+                    <button className={Style.get} onClick={() => handleButtonClick('get')}>Get Books</button>
                 </div>
 
-                <div className={Style.BookLendingForm}>
-                    <h2>Book Lending Form</h2>
-                    <p>Member ID: {selectedMember ? selectedMember.userID : "Not selected"}</p>
-                    <p>Book Name: {selectedBook ? selectedBook.title : "Not selected"}</p>
-                    <button onClick={handleLendBook}>Lend Book</button>
+                <div className={Style.displayComponent}>
+                    {displayComponent === 'lend' && <AdminBookIssue />}
+                    {displayComponent === 'get' && <AdminBookGetReturn />}
                 </div>
-
-                <AlertMessage
-                    show={showError}
-                    message="Please select both a member and a book to proceed."
-                    onClose={handleCloseError}
-                />
-
             </div>
 
             <Footer />
