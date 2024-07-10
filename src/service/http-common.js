b9d3210e213b04bd09b5c35fb13ff2a06c8a0e27
shobikan <shobikanv.21@uom.lk>
2024-07-10 09:41:55 +0530

    Create BaseUrl.js

--- src/service/http-common.js
+++ src/service/http-common.js
@@ -1,8 +1,45 @@
 import axios from "axios";
+import jwtDecode from "jwt-decode";
+import dayjs from "dayjs";
+import { useNavigate } from "react-router-dom";
+import { baseURL } from "./baseURL";
 
-export default axios.create({
-    baseURL: "http://localhost:8080",
+
+
+const httpCommon = axios.create({
+    baseURL: baseURL,
     headers: {
         "Content-type": "application/json"
     }
 });
+
+httpCommon.interceptors.request.use(
+    async (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            const user = jwtDecode(token);
+            const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1;
+            if (isExpired) {
+                LogoutUser(); // Token is expired, logout the user.
+            } else {
+                config.headers.Authorization = `Bearer ${token}`;
+            }
+        } else {
+            LogoutUser(); // No token found, need to login again.
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+
+export default httpCommon;
+
+const navigate = useNavigate();
+
+export const LogoutUser = () => {
+    localStorage.removeItem('token');
+    navigate('/login')
+}
