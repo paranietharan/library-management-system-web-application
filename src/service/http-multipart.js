599532cf12eddbdc877ba3888947b86b9e15df4e
shobikan <shobikanv.21@uom.lk>
2024-07-10 09:41:55 +0530

    Create BaseUrl.js

--- src/service/http-multipart.js
+++ src/service/http-multipart.js
@@ -1,10 +1,44 @@
 import axios from "axios";
+import jwtDecode from "jwt-decode";
+import dayjs from "dayjs";
+import { useNavigate } from "react-router-dom";
+import { baseURL } from "./baseURL";
 
 const httpMultipart = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: baseURL,
     headers: {
         "Content-type": "multipart/form-data"
     }
 });
 
+httpMultipart.interceptors.request.use(
+    async (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            const user = jwtDecode(token);
+            const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1;
+            if (isExpired) {
+                LogoutUser(); // Token is expired, logout the user.
+            } else {
+            config.headers.Authorization = `Bearer ${token}`;
+            }
+        }
+        else {
+            LogoutUser(); // No token found, need to login again.
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
 export default httpMultipart;
+
+const navigate = useNavigate();
+
+export const LogoutUser = () => {
+    localStorage.removeItem('token');
+    navigate('/login')
+}
+
