09e00258c1d47043d72d76757f1c8482ece35f1c
Paranietharan <paranietharan64@gmail.com>
2024-07-11 23:50:23 +0530

    User search created

--- src/Components/EditMemberComponent.js
+++ src/Components/EditMemberComponent.js
@@ -1,9 +1,24 @@
-// EditMemberComponent
+import styles from './style/EditMemberComponent.module.css';
+import UserSearchComponent from './UserSearchComponent';
+import { useState } from 'react';
 
 function EditMemberComponent() {
+
+    const [selectedUser, setSelectedUser] = useState(null);
+
+    const handleSelectUser = (user) => {
+        setSelectedUser(user);
+    };
+
     return (
-        <div>
-            <h1>Edit Member</h1>
+        <div className={styles.EditMemberComponent}>
+            <div className={styles.title}>
+                <h1>Edit Member</h1>
+            </div>
+
+            <div className={styles.userSearch}>
+                <UserSearchComponent onSelectUser={handleSelectUser} />
+            </div>
         </div>
     );
 }
