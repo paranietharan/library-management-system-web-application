44108af7e81385b24465e6c854399013776d8c59
Paranietharan <paranietharan64@gmail.com>
2024-07-15 14:29:31 +0530

    Chat

--- src/Pages/LibrarianChat.js
+++ src/Pages/LibrarianChat.js
@@ -1,47 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import LibrarianTopNavBar from '../Components/LibrarianTopNavBar';
-import ChatPanel from '../Components/ChatPanel';
-import ChatList from '../Components/ChatList';
 import styles from './style/LibrarianChatStyle.module.css';
 import Footer from '../Components/LibraryFooter';
+import AdminChat from '../Components/AdminChat';
 
 function LibrarianChat() {
-    // State to manage selected chat
-    const [selectedChat, setSelectedChat] = useState(null);
-
-    // Dummy data for chat list
-    const chatList = [
-        { id: 1, name: 'John Doe', message: "Hey there! How are you?", imgSrc: "https://example.com/avatar.jpg"},
-        { id: 2, name: 'Jane Smith', message: "Hey there! How are you?", imgSrc: "https://example.com/avatar.jpg" },
-        { id: 3, name: 'Alice Johnson', message: "Hey there! How are you?", imgSrc: "https://example.com/avatar.jpg" },
-        { id: 4, name: 'John Doe', message: "Hey there! How are you?", imgSrc: "https://example.com/avatar.jpg"},
-        { id: 5, name: 'Jane Smith', message: "Hey there! How are you?", imgSrc: "https://example.com/avatar.jpg" },
-        { id: 6, name: 'Alice Johnson', message: "Hey there! How are you?", imgSrc: "https://example.com/avatar.jpg" }
-    ];
-
-    // Handler function to select a chat
-    const handleChatSelect = (chatId) => {
-        setSelectedChat(chatId);
-    };
-
     return (
-        <div className={styles.librarianChat}>
+        <div className={styles.AdminChatPage}>
             <LibrarianTopNavBar />
-
-            <div className={styles.Container}>
-                <div className={styles.chatContainer}>
-                    <div className={styles.chatList}>
-                        <ChatList chats={chatList} onSelect={handleChatSelect} />
-                    </div>
-
-                    <div className={styles.chatPannel} >
-                        <ChatPanel selectedChat={selectedChat} />
-                    </div>
-                </div>
+            <div className={styles.AdminChatContainer}>
+                <AdminChat />
             </div>
-
             <Footer />
-        </div >
+        </div>
     );
 }
 
