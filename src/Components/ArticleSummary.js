74c000908313e596f5f22c3dc62a5a78cdd8a145
Paranietharan <paranietharan64@gmail.com>
2024-07-18 13:03:07 +0530

    Article Home

--- src/Components/ArticleSummary.js
+++ src/Components/ArticleSummary.js
@@ -13,7 +13,7 @@ function ArticleSummary({ article }) {
   const displayedTitle = title.length > 7 ? `${title.slice(0, 7).join(' ')}...` : title.join(' ');
 
   return (
-    <Card sx={{ maxWidth: '100%', maxHeight: '400px', margin: '10px', backgroundColor: '#e6e6e6' }}>
+    <Card sx={{ maxWidth: '100%', maxHeight: '400px', margin: '10px', backgroundColor: '#e6e6e6' , border: '2px solid #cccccc'}}>
       <Link to={`/article/${article.articleID}`} style={{ textDecoration: 'none', color: 'inherit' }}>
         {/* Use article.articleID instead of article.articleId */}
         <CardActionArea>
@@ -22,24 +22,24 @@ function ArticleSummary({ article }) {
             height="140"
             image={`data:image/jpeg;base64,${article.articleImg}`}
             alt="Article Image"
-            style={{ width: '25vw', maxHeight: '250px', height: 'auto' }}
+            style={{ width: '25vw', maxHeight: '250px', height: 'auto', border: '2px solid #b3b3b3', objectFit: 'cover' }}
           />
           <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
+            <Typography gutterBottom variant="h5" component="div" sx={{fontFamily: '"Oswald", sans-serif',}}>
               {displayedTitle}
             </Typography>
-            <Typography variant="body2" color="text.secondary">
+            <Typography variant="body2" color="text.secondary" sx={{fontFamily: '"Playwrite NO"', fontWeight: '1000'}}>
               {article.body.substring(0, 200)} {/* Display first 200 characters of body */}
             </Typography>
           </CardContent>
         </CardActionArea>
       </Link>
-      <CardActions>
+      {/* <CardActions>
         <Button size="small">Share</Button>
         <Button size="small" component={Link} to={`/article/${article.articleID}`} style={{ textDecoration: 'none' }}>
           Read Article
         </Button>
-      </CardActions>
+      </CardActions> */}
     </Card>
   );
 }
